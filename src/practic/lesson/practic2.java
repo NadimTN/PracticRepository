687498b78ff9b3471722dd76bc9db3b5a454cf9d
NadimNT <nadimtaghiyev@gmail.com>
2025-06-27 13:14:15 -0400

    Coflict addition

--- src/practic/lesson/practic2.java
+++ src/practic/lesson/practic2.java
@@ -4,7 +4,9 @@ public class practic2 {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("second practic file");
+		System.out.println("second practic file Conflict ");
+		
+		System.out.println("Conflict line");
 	}
 
 }
