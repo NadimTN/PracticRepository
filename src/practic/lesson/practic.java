f7d99a432eebbdd821b91f21b1980df94c48b869
NadimNT <nadimtaghiyev@gmail.com>
2025-06-27 11:20:24 -0400

    Practic2 file added first one

--- src/practic/lesson/practic.java
+++ src/practic/lesson/practic.java
@@ -5,6 +5,8 @@ public class practic {
 	
 	public static void main(String[] args) {
 		 System.out.println("Hello world");
+		 
+		 System.out.println("second  line");
 	}
 
 }
---
+++ src/practic/lesson/practic2.java
@@ -0,0 +1,10 @@
+package practic.lesson;
+
+public class practic2 {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("second practic file");
+	}
+
+}
