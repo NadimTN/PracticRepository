18d1bdbcfb0ab8dacf80ec563fb9028e4390516e
NadimNT <nadimtaghiyev@gmail.com>
2025-06-27 13:22:08 -0400

    Try to create Conflict second time

--- src/practic/lesson/practic2.java
+++ src/practic/lesson/practic2.java
@@ -4,9 +4,9 @@ public class practic2 {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("second practic file Conflict ");
+		System.out.println("second practic file try to create conflict again ");
 		
-		System.out.println("Conflict line");
+		System.out.println("Conflict again line line");
 	}
 
 }
